1fb83571f56cc63b6a44b86c6d413a20cef5207f
Charles Rymal <charlesrymal@gmail.com>
2013-11-14 11:08:01 -0800

    notes

--- box-1/src/main.cpp
+++ box-1/src/main.cpp
@@ -1,3 +1,16 @@
+/* TODO
+ * - create a JSL::Master class. On init, finds available devices and makes a list.
+ * - scene setup using xml
+ * - cleanly destroy actors and scenes (so that scene can be reset in-game)
+ * - automate connection of camera (and other objects) to devices (via JSL::Master)
+ * - cleanly exit (JSL read threads, glut windows)
+ * - create terminal (via GUL)
+ * - make command line revolve around objects (ex. command to change camera mode or target would be directed to the camera)
+ *   - help command to print list of objects listening to terminal
+ *   - another idea: have program listen to actual terminal (/dev/ttyX)
+ * - focus tracking for windows to suspend signals when appropriate
+ */
+
 #include <functional>
 
 #include <GRU/Master.h>
@@ -13,12 +26,12 @@
 
 int	main(int argc, char const ** argv)
 {
-	
-	
-	
+
+
+
 	// NEB
 	NEB::physics.Init();
-	
+
 	NEB::Scene* scene = NEB::physics.Create_Scene();
 
 	NEB::View * view = new NEB::View;
@@ -26,46 +39,46 @@ int	main(int argc, char const ** argv)
 
 	// Actors
 
-/*	NEB::Actor::Rigid_Dynamic_Box* box =		*/scene->Create_Light();
+	/*	NEB::Actor::Rigid_Dynamic_Box* box =		*/scene->Create_Light();
 
-		
+
 	NEB::Actor::Rigid_Dynamic_Box* box =		scene->Create_Rigid_Dynamic_Box();
 	box->pose_ = physx::PxTransform( physx::PxVec3(2.0f, 0.0f, 0.0f) );
 
 
 	NEB::Actor::Rigid_Static_Plane* plane =		scene->Create_Rigid_Static_Plane();
 	plane->pose_ = physx::PxTransform( physx::PxVec3(0.0f, -5.0f, 0.0f) );
-	
-	
-	
-	
+
+
+
+
 	// GRU
-	
+
 	GRU::Window * window = new GRU::Window(
 			&GRU::master,
 			600, 600,		// height, width
 			200, 100,		// initPosition (x,y)
 			"First Window" );	// title
-	
+
 	// main
-	
+
 	camera->SetWindow(window);
 	camera->view_ = view;
-	
-	
-	
-	
+
+
+
+
 	view->scene_ = scene;
 
-	
-	
+
+
 	window->StartSpinning();
 
 
 
 	// JSL
 	JSL::Event ev_mouse, ev_keyboard;
-	
+
 	ev_mouse.Open("/dev/input/event6");
 	ev_keyboard.Open("/dev/input/event7");
 
@@ -74,18 +87,18 @@ int	main(int argc, char const ** argv)
 	ev_mouse.map_sig_rel_[REL_X].connect(	std::bind( &NEB::Camera::FirstOrderDeltaYawRel,		camera, std::placeholders::_1 ) );
 	ev_mouse.map_sig_rel_[REL_Y].connect(	std::bind( &NEB::Camera::FirstOrderDeltaPitchRel,	camera, std::placeholders::_1 ) );
 	ev_keyboard.sig_key_.connect( 		std::bind( &NEB::Camera::HandleKey,			camera, std::placeholders::_1, std::placeholders::_2 ) );
-	
+
 	ev_mouse.Launch();
 	ev_keyboard.Launch();
 
-	
+
 	// Run
 
 	GRU::master.CallGlutMainLoop();
-	
+
 	ev_mouse.Join();
 	ev_keyboard.Join();
-	
+
 	return 0;
 }
 
