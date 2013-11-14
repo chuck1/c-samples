658012b2b83a28abcd20b5fc9aeded5b86ae1b6d
Charles Rymal <charlesrymal@gmail.com>
2013-11-13 23:55:44 -0800

    update;

--- box-1/src/main.cpp
+++ box-1/src/main.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 
 #include <GRU/Master.h>
 #include <GRU/Window.h>
@@ -6,38 +7,87 @@
 #include <NEB/Scene.h>
 #include <NEB/Actor/Rigid_Dynamic_Box.h>
 #include <NEB/View.h>
+#include <NEB/Camera.h>
 
-int	main()
+#include <JSL/Event.h>
+
+int	main(int argc, char const ** argv)
 {
+	
+	
 	
 	// NEB
 	NEB::physics.Init();
 	
 	NEB::Scene* scene = NEB::physics.Create_Scene();
-	
-	NEB::Actor::Rigid_Dynamic_Box* box = scene->Create_Rigid_Dynamic_Box();
 
 	NEB::View * view = new NEB::View;
+	NEB::Camera * camera = new NEB::Camera;
+
+	// Actors
+
+/*	NEB::Actor::Rigid_Dynamic_Box* box =		*/scene->Create_Light();
+
+		
+	NEB::Actor::Rigid_Dynamic_Box* box =		scene->Create_Rigid_Dynamic_Box();
+	box->pose_ = physx::PxTransform( physx::PxVec3(2.0f, 0.0f, 0.0f) );
+
+
+	NEB::Actor::Rigid_Static_Plane* plane =		scene->Create_Rigid_Static_Plane();
+	plane->pose_ = physx::PxTransform( physx::PxVec3(0.0f, -5.0f, 0.0f) );
+	
+	
+	
 	
 	// GRU
 	
 	GRU::Window * window = new GRU::Window(
 			&GRU::master,
-			200, 200,		// height, width
+			600, 600,		// height, width
 			200, 100,		// initPosition (x,y)
 			"First Window" );	// title
-
 	
 	// main
 	
-	view->SetWindow(window);
+	camera->SetWindow(window);
+	camera->view_ = view;
+	
+	
+	
+	
 	view->scene_ = scene;
+
+	
 	
 	window->StartSpinning();
 
+
+
+	// JSL
+	JSL::Event ev_mouse, ev_keyboard;
+	
+	ev_mouse.Open("/dev/input/event6");
+	ev_keyboard.Open("/dev/input/event7");
+
+	//ev_mouse.Set( JSL::Event::flag::PRINT );
+
+	ev_mouse.map_sig_rel_[REL_X].connect(	std::bind( &NEB::Camera::FirstOrderDeltaYawRel,		camera, std::placeholders::_1 ) );
+	ev_mouse.map_sig_rel_[REL_Y].connect(	std::bind( &NEB::Camera::FirstOrderDeltaPitchRel,	camera, std::placeholders::_1 ) );
+	ev_keyboard.sig_key_.connect( 		std::bind( &NEB::Camera::HandleKey,			camera, std::placeholders::_1, std::placeholders::_2 ) );
+	
+	ev_mouse.Launch();
+	ev_keyboard.Launch();
+
+	
+	// Run
+
 	GRU::master.CallGlutMainLoop();
+	
+	ev_mouse.Join();
+	ev_keyboard.Join();
 	
 	return 0;
 }
+
 
 
