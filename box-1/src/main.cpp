09f28813e456a440ffd5e1e72317ca51a03fbb30
Charles Rymal <charlesrymal@gmail.com>
2013-11-16 01:34:34 -0800

    automated commit

--- box-1/src/main.cpp
+++ box-1/src/main.cpp
@@ -1,6 +1,4 @@
 /* TODO
- * - create a JSL::Master class. On init, finds available devices and makes a list.
- * - scene setup using xml
  * - cleanly destroy actors and scenes (so that scene can be reset in-game)
  * - automate connection of camera (and other objects) to devices (via JSL::Master)
  * - cleanly exit (JSL read threads, glut windows)
@@ -36,25 +34,19 @@ int	main(int argc, char const ** argv)
 	// NEB
 	NEB::physics.Init();
 
-	NEB::Scene* scene = NEB::physics.Create_Scene();
-
+	TiXmlDocument document("scene.xml");
+	if ( !document.LoadFile() )
+	{
+		printf ("XML file not found\n");
+		return 0;
+	}
+	
+	TiXmlElement* el_scene = document.FirstChildElement("scene");
+	
+	std::shared_ptr<NEB::Scene> scene = NEB::physics.Create_Scene(el_scene);
+	
 	NEB::View * view = new NEB::View;
 	NEB::Camera * camera = new NEB::Camera;
-
-	// Actors
-
-	/*	NEB::Actor::Rigid_Dynamic_Box* box =		*/scene->Create_Light();
-
-
-	NEB::Actor::Rigid_Dynamic_Box* box =		scene->Create_Rigid_Dynamic_Box();
-	box->pose_ = physx::PxTransform( physx::PxVec3(2.0f, 0.0f, 0.0f) );
-
-
-	NEB::Actor::Rigid_Static_Plane* plane =		scene->Create_Rigid_Static_Plane();
-	plane->pose_ = physx::PxTransform( physx::PxVec3(0.0f, -5.0f, 0.0f) );
-
-
-
 
 	// GRU
 
@@ -71,7 +63,7 @@ int	main(int argc, char const ** argv)
 
 	camera->Connect();
 
-
+	
 	view->scene_ = scene;
 
 
@@ -82,7 +74,7 @@ int	main(int argc, char const ** argv)
 
 
 	// Run
-	
+
 	JSL::master.Launch();
 
 	GRU::master.CallGlutMainLoop();
