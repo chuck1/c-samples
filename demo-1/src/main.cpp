a022ce9f560df2a65690b564ece3650fc5a7522e
Charles Rymal <charlesrymal@gmail.com>
2013-12-02 17:35:45 -0800

    update

--- demo-1/src/main.cpp
+++ demo-1/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <glutpp/master.h>
 #include <glutpp/window.h>
+#include <glutpp/sphere.h>
 
 class window:
 	public glutpp::window
@@ -10,11 +11,27 @@ public:
 	window( int w, int h, int x, int y, const char * title):
 		glutpp::window(w,h,x,y,title)
 	{
-		camera_.eye_ = math::vec4(0,5,-5,0);
+		camera_.eye_ = math::vec4(0,4,4,0);
 		
-		light_.camera_.eye_ = math::vec4(0,5,0,0);
+		//flags_ |= PLANAR_REFLECTION;
+		flags_ |= SHADOW | SHADOW_TEXTURE;
+		
+		// light
+		glutpp::light* light = new glutpp::light(this, GL_LIGHT0);
+		light->camera_.eye_ = math::vec4(0,5,5,0);
+		
+		// sphere
+		glutpp::sphere* sphere = new glutpp::sphere(this);
+
+		// plane
+		glutpp::plane* plane = new glutpp::plane(this);
+		plane->plane_.n = math::vec3(0.3,1.0,0.0);
+		
+		objects_.push_back(light);
+		objects_.push_back(sphere);
+		objects_.push_back(plane);
 	}
-	void	Display()
+/*	void	Display()
 	{
 		glMaterialfv(GL_FRONT, GL_AMBIENT, math::white);
 		glMaterialfv(GL_FRONT, GL_DIFFUSE, math::blue);
@@ -26,7 +43,7 @@ public:
 		glTranslatef(2,0,0);
 		
 		glutSolidCube(1.0);
-	}
+	}*/
 };
 
 int	main()
