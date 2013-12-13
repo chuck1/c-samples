932b3b2f4f1b1bf6f4167e17b762c0cf432ae776
Charles Rymal <charlesrymal@gmail.com>
2013-12-13 02:22:55 -0800

    update

--- box-1/src/main.cpp
+++ box-1/src/main.cpp
@@ -1,4 +1,4 @@
-/* TODO
+/* 
  * - cleanly destroy actors and scenes (so that scene can be reset in-game)
  * - automate connection of camera (and other objects) to devices (via JSL::Master)
  * - cleanly exit (JSL read threads, glut windows)
@@ -19,6 +19,7 @@
 
 #include <neb/physics.h>
 #include <neb/scene.h>
+#include <neb/actor/Rigid_Body.h>
 #include <neb/actor/Rigid_Dynamic.h>
 #include <neb/view.h>
 #include <neb/camera.h>
@@ -27,20 +28,25 @@ class window: public glutpp::window
 {
 	public:
 		window(int,int,int,int,char const *);
-		void	step(double);
+		void		step(double);
 
-		neb::view* view_;
-		neb::scene* scene_;
+		neb::view*	view_;
+		neb::scene*	scene_;
 };
 window::window(int w, int h, int x, int y, char const * title):
-	glutpp::window(w,h,x,y,title)
+	glutpp::window(w,h,x,y,title),
+	view_(NULL),
+	scene_(NULL)
+
 {
 
 }
 void	window::step(double time)
 {
 	printf("%s\n", __PRETTY_FUNCTION__);
-	
+
+	assert(scene_);
+
 	scene_->step(time);
 }
 int	main(int argc, char const ** argv)
@@ -59,11 +65,8 @@ int	main(int argc, char const ** argv)
 
 	TiXmlElement* el_scene = document.FirstChildElement("scene");
 
-	printf("create scene\n");
 
 
-	
-	
 	printf("window\n");
 
 	window w(600, 600, 200, 100, "First Window");
@@ -95,14 +98,83 @@ int	main(int argc, char const ** argv)
 
 	w.add_light(&l);
 
+	// select our lead actor
+	neb::actor::Rigid_Dynamic* actor = (neb::actor::Rigid_Dynamic*)w.scene_->actors_.at(0);
 	
 	std::shared_ptr<neb::camera_ridealong> ride(new neb::camera_ridealong);
-	ride->actor_ = w.scene_->actors_.at(0);
+	ride->actor_ = actor;
 	
-	
+	w.map_sig_key_[GLFW_KEY_SPACE].connect(std::bind(
+		&neb::actor::Rigid_Body::key_force,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3(0.0, 1.0, 0.0)));
+	w.map_sig_key_[GLFW_KEY_W].connect(std::bind(
+		&neb::actor::Rigid_Body::key_force,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3(0.0, 0.0,-1.0)));
+	w.map_sig_key_[GLFW_KEY_S].connect(std::bind(
+		&neb::actor::Rigid_Body::key_force,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3(0.0, 0.0, 1.0)));
+	w.map_sig_key_[GLFW_KEY_D].connect(std::bind(
+		&neb::actor::Rigid_Body::key_force,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3( 1.0, 0.0, 0.0)));
+	w.map_sig_key_[GLFW_KEY_A].connect(std::bind(
+		&neb::actor::Rigid_Body::key_force,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3(-1.0, 0.0, 0.0)));
+
+
+
+	w.map_sig_key_[GLFW_KEY_I].connect(std::bind(
+		&neb::actor::Rigid_Body::key_torque,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3(-1.0, 0.0, 0.0)));
+	w.map_sig_key_[GLFW_KEY_K].connect(std::bind(
+		&neb::actor::Rigid_Body::key_torque,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3( 1.0, 0.0, 0.0)));
+	w.map_sig_key_[GLFW_KEY_L].connect(std::bind(
+		&neb::actor::Rigid_Body::key_torque,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3( 0.0,-1.0, 0.0)));
+	w.map_sig_key_[GLFW_KEY_J].connect(std::bind(
+		&neb::actor::Rigid_Body::key_torque,
+		actor,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3,
+		math::vec3( 0.0, 1.0, 0.0)));
+
+
 	printf("control\n");
 	w.camera_.control_ = ride;
-	
+
 	printf("loop\n");
 	w.loop();
 
