ee4a50e4e9e2542a1c05e1d4218134fe15dfcc54
Charles Rymal <charlesrymal@gmail.com>
2013-12-18 14:46:37 -0800

    auto

--- box-1/src/main.cpp
+++ box-1/src/main.cpp
@@ -18,6 +18,8 @@
 #include <glutpp/window.h>
 #include <glutpp/renderable.h>
 #include <glutpp/scene.h>
+#include <glutpp/gui/object/object_factory.h>
+#include <glutpp/gui/object/textview.h>
 
 #include <neb/app.h>
 #include <neb/user.h>
@@ -28,42 +30,120 @@
 #include <neb/view.h>
 #include <neb/camera.h>
 
-enum box
+
+
+namespace box
 {
-	SCENE_0,
-	LAYOUT_0
-};
+	enum box
+	{
+		SCENE_0,
+		LAYOUT_HOME,
+		LAYOUT_GAME
+	};
 
+	namespace layouts
+	{
+		namespace home
+		{
+			class local_game: public glutpp::gui::object::textview
+			{
+				public:
+					virtual int	mouse_button_fun(int button, int action, int mods)
+					{
+						return 1;
+					}
+			};
+			class network_game: public glutpp::gui::object::textview
+			{
+				public:
+					virtual int	mouse_button_fun(int button, int action, int mods)
+					{
+						return 1;
+					}
+			};
+		}
+		namespace game
+		{
+			class exit: public glutpp::gui::object::textview
+			{
+				public:
+					virtual int	mouse_button_fun(int button, int action, int mods)
+					{
+						return 1;
+					}
+			};
+		}
+	}
+	class object_factory: public glutpp::gui::object::object_factory
+	{
+		public:
+			virtual std::shared_ptr<glutpp::gui::object::object>	create(TiXmlElement* element) {
 
+				assert(element);
+
+				std::shared_ptr<glutpp::gui::object::object> object;
+
+				char const * buf = element->Attribute("type");
+				assert(buf);
+
+				if(strcmp(buf, "home_local_game") == 0)
+				{
+					object.reset(new layouts::home::local_game);
+				}
+				else if(strcmp(buf, "home_network_game") == 0)
+				{
+					object.reset(new layouts::home::network_game);
+				}
+				else if(strcmp(buf, "game_exit") == 0)
+				{
+					object.reset(new layouts::game::exit);
+				}
+				else
+				{
+					object = glutpp::gui::object::object_factory::create(element);
+				}
+
+				object->load_xml(element);
+
+				return object;
+			}
+	};
+}
 
 int	main(int argc, char const ** argv)
 {
 	neb::__physics.Init();
-	
+
+	glutpp::__master.object_factory_.reset(new box::object_factory);
+
 	std::shared_ptr<neb::app> app(new neb::app);
 	app->init();
+
 	app->load_scene(box::SCENE_0, "scene.xml");
-	
-	//app->activate_scene(box::SCENE_0);
+	app->load_layout(box::LAYOUT_HOME, "layout.xml");
+	app->load_layout(box::LAYOUT_GAME, "layout_game.xml");
+
+	app->activate_scene(box::SCENE_0);
+	app->activate_layout(box::LAYOUT_GAME);
 	
 	
 	
 	std::shared_ptr<neb::user> user(new neb::user);
-	
+
 	auto actor = std::dynamic_pointer_cast<neb::actor::Base>(app->scenes_[box::SCENE_0]->actors_.at(0));
-	
-	
+
+
 	user->set_actor(actor, neb::camera_type::e::RIDEALONG);
 	user->connect(app->window_);
-	
-	
-	
-	
+
+
+
+
 	printf("loop\n");
 	app->window_->prepare();
 	app->window_->loop();
-	
-	
+
+
 	return 0;
 }
 
